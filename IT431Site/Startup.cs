c4646c8bf72b37912b68b8352e90a914781f7b26
jcole <jcole@DESKTOP-Q7EQC78>
2017-04-20 20:18:27 -0400

    Add project files.

---
+++ IT431Site/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IT431Site.Startup))]
+namespace IT431Site
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
